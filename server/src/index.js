6d4b7c501ff59e102f6436efcc8913f28f3e0e82
blackeye75 <priyanshuraj22275@gmail.com>
2024-07-29 01:19:54 +0530

    db CONNECTION

---
+++ server/src/index.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+// (async () => {
+//   try {
+//     console.log(process.env.MONGODB_URI);
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`APP IS LISTENING ON PORT ${process.env.PORT}`);
+//     })
+//   } catch (error) {
+//     console.error("ERROR:", error);
+//     throw error;
+//   }
+// })();
