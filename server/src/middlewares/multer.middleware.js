6fe45283fb97f90cd11b541f1bdcbf189ab2a062
blackeye75 <priyanshuraj22275@gmail.com>
2024-08-01 11:51:33 +0530

    multer cloudinary setup

---
+++ server/src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import { multer } from "multer";
+
+const storge = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/public/tmp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.orignalName);
+  },
+});
+
+export const upload = multer({ storge });
