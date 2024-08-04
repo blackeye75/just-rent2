50a42784e0e1d9e352d642a96f2e75360415e2cb
blackeye75 <priyanshuraj22275@gmail.com>
2024-08-05 01:17:41 +0530

    user routes created

---
+++ server/src/controllers/property.controller.js
@@ -0,0 +1,11 @@
+import Property from "../models/property.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const createProperty = asyncHandler(async (req, res) => {
+    
+});
+
+export { createProperty };
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import {
+  chnageCurrentPassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifiyJWT } from "../middlewares/auth.middleware.js";
@@ -12,6 +16,9 @@ const router = Router();
 router.route("/register").post(upload.single("avatar"), registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifiyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-Password").post(verifiyJWT, chnageCurrentPassword);
+router.route("/current-user").get(verifiyJWT, getCurrentUser);
+router.route("/update-account").patch(verifiyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifiyJWT, upload.single("avatar"), updateUserAvatar);
 export default router;
